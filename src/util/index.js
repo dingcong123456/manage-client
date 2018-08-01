11d55b25f8b30681ad181f9c6f6dbf638ac8b009
dingcong123456 <18842884125@163.com>
2018-08-01 11:06:46 +0800

    添加图片缩略图后缀

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,4 @@
 export function qiniuUrl(hash){
-    if (hash.indexOf('http')!=-1) return hash;
+    if (hash.indexOf('http')!=-1) return `${hash}?imageView2/2/w/1024/h/1024`;
     return `http://pc43w2wc6.bkt.clouddn.com/${hash}`;
 }
